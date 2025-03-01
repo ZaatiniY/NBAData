5148054d6c830ea0a807a431ed7cd640265d84f3
Yaser Zaatini <zaatiniy@gmail.com>
2025-03-01 10:26:09 -0600

    continued to build search tree for navigating basketball reference website for seasons. Still building regular season table searches

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ import util
 import urllib.request
 from urllib.error import HTTPError
 import time
+import pandas as pd 
+import numpy as np
 
 
 startLink = 'https://www.basketball-reference.com/leagues/'
@@ -13,12 +15,14 @@ class Scrape():
         self.startLink = website
         self.lag = 1
         self.counter = 0
+        self.state = "START"
     
     def simRun(self):
         htmlStart = util.readWebPageHTML(startLink)
         soupStart= util.transformToSoupHP(htmlStart)
         seasons = soupStart.find_all(attrs = {"data-stat":"season"})
         links = self.getSeasonLinks(seasons)
+        print(links)
         correctedLinks = self.editSeasonLinks(links) 
         self.goThroughSeasonLinks(correctedLinks,test = 1)
 
@@ -28,17 +32,25 @@ class Scrape():
         index = 0
         while index < len(seasonLinks) and index< test:
             try:
+                input(f"This is the current season {seasonLinks[index]}")
+                print(index)
                 time.sleep(3)
                 htmlSeason = util.readWebPageHTML(seasonLinks[index])
                 seasonSoup = util.transformToSoupHP(htmlSeason)
-                statRows = util.ppgRegTableSearch(seasonSoup)
+                statRows = util.posRegTableSearch(seasonSoup)
                 print(statRows)
+                #print(statRows[0].find_all('td')[4].string) #just testing how to use .string to get the value in the table 
+                
                 self.adjustSiteCount(index)
                 index += 1
+                
 
             except HTTPError as e:
                 if e == 429:
-                    print("HTTP ERROR LMAO ")
+                    print("fail")
+
+    def playoffsDataPull(self,index):
+        pass
 
     def adjustSiteCount(self,index):
         pass
@@ -50,11 +62,11 @@ class Scrape():
         return links
 
     def editSeasonLinks(self,baseLinks):
-        newLinks = [(self.startLink + '/' + x) for x in baseLinks]
+        newLinks = [('https://www.basketball-reference.com' + x) for x in baseLinks]
         return newLinks 
 
 sim = Scrape(startLink)
-print(sim.simRun())
+sim.simRun()
 
 
 
--- util.py
+++ util.py
@@ -1,14 +1,20 @@
 from bs4 import BeautifulSoup
 import time 
 from urllib.request import urlopen
+import numpy as np
 
 #readWebPageHTML will give you the HTML version of a website
 #   parameters:
 #       - link - string; website link 
 #   return->    htmlTree - interpretted website link as the proper html format
+def printState(state):
+    print("WHY ARENOT YOU PRINTING")
+    input(f"You are currently in {state} state; hit enter to continue")
+
 def readWebPageHTML(link):
     with urlopen(link) as response:
         htmlTree = response.read()
+        #print(f"{htmlTree}")
     return htmlTree
 
 def transformToSoupHP(html):
@@ -19,17 +25,44 @@ def searchTreeTagAll(soup,tag):
     results = soup.find_all(tag)
     return results
 
-
 def searchTreeClassAll(soup,c):
     results = soup.find_all(class_ = c)
     return results
 
+#posRegTableSearch is taking the Soup Object for each individual season being looped through in test.py
+# From there, we will search the soup for all the data rows and columns
+# Return --->   tableDF - pandas DF with dimension that match those of "Per 100 Pos" for the analyzed season; removing unnecessary components  
+def posRegTableSearch(soup):
+    tableRows = getPosDataRows(soup)
+    #columnHeaders = getPosColumnHeaders(soup)
+    return tableRows   
 
+def getPosColumnHeaders():
+    pass
 
-def ppgRegTableSearch(soup):
-    table = soup.find_all(id="per_game-team")
-    tableRows = table[0].tbody.find_all("tr")
-    return tableRows    
+def getPosDataRows(soup):
+    tableRowTag = 'tr'
+    table = soup.find_all(id="per_poss-team")
+    rawTableRows = table[0].tbody.find_all(tableRowTag)
+    input(f"This is the length of rawTableRows: {len(rawTableRows)} - hit enter to continue ")
+    dataRows = makeSoupSearchNP(rawTableRows)
+    return dataRows
 
+def makeSoupSearchNP(soupResults):
+    relevantDataTag = 'td'
+    compiledData = np.zeros(createZerosNPArray(soupResults))
+    for index in range(len(soupResults)):
+        newRow = [rowElement.string for rowElement in soupResults[index].find_all(relevantDataTag)]
+        compiledData[index] = newRow
+    return compiledData  
+
+def createZerosNPArray(soupResults):
+    relevantDataTag = 'td'
+    rowNumber = len(soupResults)
+    columnNumber= len([columnElement for columnElement in soupResults[0].find_all(soupResults)]) #CHANGE - find a way that this can make legible; seems silly this is the best way to get column count
+    return [rowNumber,columnNumber]
+
+def makePandasDF(columnNames, rowData):
+    pass
 
       
