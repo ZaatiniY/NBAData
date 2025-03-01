766e61ff5959fee8f7313e3d190c03cde134f2c9
Yaser Zaatini <zaatiniy@gmail.com>
2025-03-01 17:46:30 -0600

    regular season data frames are not being made for first season, but struggling to have them made for next seasons

--- util.py
+++ util.py
@@ -2,14 +2,12 @@ from bs4 import BeautifulSoup
 import time 
 from urllib.request import urlopen
 import numpy as np
+import pandas as pd
 
 #readWebPageHTML will give you the HTML version of a website
 #   parameters:
 #       - link - string; website link 
 #   return->    htmlTree - interpretted website link as the proper html format
-def printState(state):
-    print("WHY ARENOT YOU PRINTING")
-    input(f"You are currently in {state} state; hit enter to continue")
 
 def readWebPageHTML(link):
     with urlopen(link) as response:
@@ -32,37 +30,40 @@ def searchTreeClassAll(soup,c):
 #posRegTableSearch is taking the Soup Object for each individual season being looped through in test.py
 # From there, we will search the soup for all the data rows and columns
 # Return --->   tableDF - pandas DF with dimension that match those of "Per 100 Pos" for the analyzed season; removing unnecessary components  
-def posRegTableSearch(soup):
-    tableRows = getPosDataRows(soup)
-    #columnHeaders = getPosColumnHeaders(soup)
-    return tableRows   
+def regTableSearch(soup,specifiedID):
+    columnHeaders = getRegColumnHeaders(soup,specifiedID)  
+    tableRows = getRegDataRows(soup,specifiedID)
+    headerIndexExcess = len(columnHeaders) - len(tableRows[0]) #tableRows is a list of lists, therefore to get how many items are in an individual table row, you need to look at an individual index (in this case zero)
+    input(f"This is the base results of the table headers - {columnHeaders}")
+    columnHeadersCorrected = columnHeaders[headerIndexExcess:] #this should remove the nonrelavant column search results from basketball reference tables
+    input(f"This is the corrected results of the table headers - {columnHeadersCorrected}")
+    perPosDF = pd.DataFrame(tableRows,columns=columnHeadersCorrected)
+    return perPosDF   
 
-def getPosColumnHeaders():
-    pass
+def getRegColumnHeaders(soup,specifiedID):
+    tableColumnsTag = 'th'
+    table = soup.find_all(id=specifiedID)
+    rawTableColumns = table[0].thead.find_all(tableColumnsTag)
+    columnHeaders = [columnName.string for columnName in rawTableColumns] 
+    #columnHeaders = removeNoneValues(columnHeaders)
+    return columnHeaders
 
-def getPosDataRows(soup):
+def getRegDataRows(soup,specifiedID):
     tableRowTag = 'tr'
-    table = soup.find_all(id="per_poss-team")
+    table = soup.find_all(id=specifiedID)
     rawTableRows = table[0].tbody.find_all(tableRowTag)
-    input(f"This is the length of rawTableRows: {len(rawTableRows)} - hit enter to continue ")
-    dataRows = makeSoupSearchNP(rawTableRows)
+    #input(f"This is the length of rawTableRows: {len(rawTableRows)} - hit enter to continue ")
+    dataRows = buildDataRows(rawTableRows)
     return dataRows
 
-def makeSoupSearchNP(soupResults):
+def buildDataRows(soupResults):
     relevantDataTag = 'td'
-    compiledData = np.zeros(createZerosNPArray(soupResults))
+    compiledData = []
     for index in range(len(soupResults)):
+        #input(f"This is your soup results for your index item - {soupResults[index]}")
         newRow = [rowElement.string for rowElement in soupResults[index].find_all(relevantDataTag)]
-        compiledData[index] = newRow
+        #newRow = removeNoneValues(newRow)
+        #input(f"This is before a new row gets added to the compiled data list of list {newRow}")
+        compiledData.append(newRow)
     return compiledData  
 
-def createZerosNPArray(soupResults):
-    relevantDataTag = 'td'
-    rowNumber = len(soupResults)
-    columnNumber= len([columnElement for columnElement in soupResults[0].find_all(soupResults)]) #CHANGE - find a way that this can make legible; seems silly this is the best way to get column count
-    return [rowNumber,columnNumber]
-
-def makePandasDF(columnNames, rowData):
-    pass
-
-      
